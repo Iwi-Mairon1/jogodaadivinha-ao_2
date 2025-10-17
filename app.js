8d8d84207799cbedb6f05e12d818faac7e64bbbd
Iwi-Mairon1 <iwi.lara@escola.pr.gov.br>
2025-10-17 11:35:14 -0300

    parte 2

---
+++ app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'escolha um numero entre 1 a 10';
+
+function exibirTextoNaTela(){
+    let campo = document.querySelector(tag);
+campo.innerHTML = texto;
+}
+
+function verificarChute(){
+    comsole.log('o botao foi clicado');
+}
+
