da75000a3380f4b9a392e474adea46b5dbc35a93
Iwi-Mairon1 <iwi.lara@escola.pr.gov.br>
2025-10-20 12:25:47 -0300

    ls

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'jogo dododo';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'jogo do numero secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'escolha um numero entre 1 a 10';
 
-function exibirTextoNaTela(){
+function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
-campo.innerHTML = texto;
+    campo.innerHTML = texto;
 }
+
+exibirtextoNaTela('h1','jogo do numero secreto');
+exibirTextoNaTela('p','escolha um numero entre 1 a 10');
 
 function verificarChute(){
     comsole.log('o botao foi clicado');
